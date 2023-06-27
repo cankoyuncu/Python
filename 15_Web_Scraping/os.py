86a9047fbcb19ff3b5a470eca5f176497677c37c
cankoyuncu <2110206542@ogrenci.karabuk.edu.tr>
2023-06-28 02:41:11 +0300

    Update os.py

--- 15_Web_Scraping/os.py
+++ 15_Web_Scraping/os.py
@@ -19,10 +19,23 @@ result = os.name
 #         print(file)
 
 #result = os.getcwd() #active location 
-
 #result = os.stat("date.py") #file information
+#result = os.stat("date.py")
+#result = datetime.datetime.fromtimestamp(result.st_mtime) #change time format
+#result = result.st_size/1024 
 
-result = os.stat("_datetime.py")
-result = result.st_size/1024 
+#os.system("notepad.exe") #open notepad
+
+#os.rmdir("newfile") #remove file if dont have subdirectory
+#os.removedirs("newfolder/newfolder2") #remove file if have subdirectory
+
+#path
+#result = os.path.abspath("date.py") #full path
+#result = os.path.dirname(os.path.abspath("date.py"))
+#result = os.path.isdir("C:\\Users\\user\\Desktop\\Python\\15_Web_Scraping\\os.py")
+#result = os.path.isfile("C:\\Users\\user\\Desktop\\Python\\15_Web_Scraping\\os.py")
+#result = os.path.join("C:\\","deneme","deneme1") #merge path
+#result = os.path.split("C:\\deneme")
+#result = os.path.splitext("os.py")
 
 print(result)
