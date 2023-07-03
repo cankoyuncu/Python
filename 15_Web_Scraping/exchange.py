777bbe906a315dad22fcba132cb5684985f1c58b
Can Koyuncu <2110206542@ogrenci.karabuk.edu.tr>
2023-07-03 23:54:07 +0300

    modified

--- 15_Web_Scraping/exchange.py
+++ 15_Web_Scraping/exchange.py
@@ -1,2 +1,49 @@
-endpoint = 'latest'
-access_key = '2a67fba54e4889447bbd52cf60fbadf6'
+# import requests
+
+# api_url = 'http://api.exchangeratesapi.io/v1/latest'
+# access_key = 'XXXXXXXXXXXXXXXX'
+
+# bozulan_doviz = input("Bozulan döviz cinsi: ")
+# alınan_doviz = input("Alınan döviz cinsi: ")
+# miktar = int(input(f"Ne kadar {bozulan_doviz} bozdurmak istiyorsunuz: "))
+
+# params = {
+#     'access_key': access_key,
+#     'base': bozulan_doviz,
+#     'symbols': alınan_doviz
+# }
+
+# response = requests.get(api_url, params=params)
+# data = response.json()
+
+# print(data)
+
+import requests
+
+# API anahtarınızı buraya girin
+api_key = "XXXXXXXXXXXXXXX"
+
+# API URL'sini oluşturun
+base_url = "https://api.exchangeratesapi.io/v1/latest"
+params = {
+    "access_key": api_key,
+    "base": "EUR",  # Giriş miktarının Euro cinsinden olduğunu varsayalım
+    "symbols": "USD"  # Dönüştürmek istediğiniz para birimi
+}
+
+# API isteğini gönderin
+response = requests.get(base_url, params=params)
+
+# API yanıtını kontrol edin
+if response.status_code == 200:
+    data = response.json()
+    if data["success"]:
+        rate = data["rates"]["USD"]
+        amount = 100  # Giriş miktarını buraya girin
+        converted_amount = amount * rate
+        print(f"{amount} EUR, {converted_amount} USD'ye eşittir.")
+    else:
+        print("API yanıtı başarısız oldu.")
+else:
+    print("API isteği başarısız oldu.")
+
