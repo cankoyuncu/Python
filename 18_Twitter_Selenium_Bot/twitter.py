3fdfb7a8739f040522b14df21fa8553e360a57d3
Can Koyuncu <2110206542@ogrenci.karabuk.edu.tr>
2023-08-22 17:04:55 +0300

    upload

---
+++ 18_Twitter_Selenium_Bot/twitter.py
@@ -0,0 +1,31 @@
+from twitterUserInfo import username, password
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+class TwitterBot:
+    def __init__(self, username, password):
+        self.browserProfile = webdriver.ChromeOptions()
+        self.browserProfile.add_experimental_option('prefs', {'intl.accept_languages':'en,en_US'})
+        self.browser = webdriver.Chrome('chromedriver.exe', options=self.browserProfile)
+        self.username = username
+        self.password = password
+
+    def signIn(self):
+        self.browser.get("https://twitter.com/i/flow/login")
+        time.sleep(2)
+
+        usernameInput = self.browser.find_element_by_xpath("//*[@id='layers']/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input")
+        # passwordInput = 
+
+        usernameInput.send_keys(self.username)
+        # passwordInput.send_keys(self.password)
+
+        btnSubmit = self.browser.find_element_by_xpath("//*[@id='layers']/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div")
+        btnSubmit.click()
+
+        time.sleep(2)
+
+twitter = TwitterBot(username, password)   
+#login
+twitter.signIn()
